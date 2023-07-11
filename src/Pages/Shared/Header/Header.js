031cdccf69bc804c4b29d635017cbb046efd2f89
eqram1 <abmeqramhossain@gmail.com>
2023-07-11 22:28:27 +0600

    Load news category data from server

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -4,11 +4,12 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import LeftSideNav from '../LeftSideNav/LeftSideNav';
+import { Link } from 'react-router-dom';
 const Header = () => {
     return (
         <Navbar collapseOnSelect className='mb-4' expand="lg" bg="light" variant="light">
             <Container>
-                <Navbar.Brand href="#home">Dragon-news</Navbar.Brand>
+                <Navbar.Brand><Link to='/'>Dragon-news</Link></Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto">
--- src/Pages/Shared/LeftSideNav/LeftSideNav.js
+++ src/Pages/Shared/LeftSideNav/LeftSideNav.js
@@ -10,7 +10,7 @@ const LeftSideNav = () => {
     }, [])
     return (
         <div>
-            <h4>All category: {categories.length}</h4>
+            <h4>All category news: {categories.length}</h4>
             <div>
                 {categories.map(category => <p key={category.id}>
                     <Link to={`/category/${category.id}`}>{category.name}</Link>
