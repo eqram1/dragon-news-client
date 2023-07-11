5464e8a129f51ff116f2624c745ff8977a8305c2
eqram1 <abmeqramhossain@gmail.com>
2023-07-11 09:27:08 +0600

    Implement Right Side Component and react ions

---
+++ src/Pages/Shared/BrandCarousel/BrandCarousel.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import editorsInsight1 from '../../../assets/editorsInsight1.png';
+import editorsInsight2 from '../../../assets/editorsInsight2.png';
+import editorsInsight3 from '../../../assets/editorsInsight3.png';
+const BrandCarousel = () => {
+    return (
+
+        <Carousel>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={editorsInsight1}
+                    alt="First slide"
+                />
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={editorsInsight2}
+                    alt="Second slide"
+                />
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={editorsInsight3}
+                    alt="Second slide"
+                />
+            </Carousel.Item>
+        </Carousel>
+
+    );
+};
+
+export default BrandCarousel;
--- src/Pages/Shared/RightSideNav/RightSideNav.js
+++ src/Pages/Shared/RightSideNav/RightSideNav.js
@@ -1,10 +1,32 @@
 import React from 'react';
-
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import { FaGoogle, FaGithub, FaFacebook, FaTwitter, FaWhatsapp, FaTwitch, FaTrowel } from "react-icons/fa6";
+import ListGroup from 'react-bootstrap/ListGroup';
+import BrandCarousel from '../BrandCarousel/BrandCarousel';
 const RightSideNav = () => {
     return (
         <div>
-            <h2>right side nav</h2>
+            <ButtonGroup vertical>
+                <Button className='mb-2' variant="outline-primary"><FaGoogle></FaGoogle>Login with Google</Button>
+                <Button variant="outline-dark"><FaGithub></FaGithub>Login with Github</Button>
+            </ButtonGroup>
+            <div className='mt-4'>
+                <h5>Find us on</h5>
+                <ListGroup>
+                    <ListGroup.Item className='mb-2'><FaFacebook></FaFacebook>Cras justo odio</ListGroup.Item>
+                    <ListGroup.Item className='mb-2'><FaTwitter></FaTwitter>Dapibus ac facilisis in</ListGroup.Item>
+                    <ListGroup.Item className='mb-2'><FaWhatsapp></FaWhatsapp>Morbi leo risus</ListGroup.Item>
+                    <ListGroup.Item className='mb-2'><FaTwitch></FaTwitch>Porta ac consectetur ac</ListGroup.Item>
+                    <ListGroup.Item className='mb-2'><FaTrowel></FaTrowel>Vestibulum at eros</ListGroup.Item>
+                </ListGroup>
+            </div>
+            <div>
+                <BrandCarousel></BrandCarousel>
+            </div>
         </div>
+
+
     );
 };
 
